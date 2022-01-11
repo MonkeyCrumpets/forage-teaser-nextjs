6036efa11c9588abaee3fa6f99e5aa718acc91a4
monkeycrumpets <brett@kluske.com>
2022-01-11 05:13:21 +0000

    Added tailwind

---
+++ components/header.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Header(props) {
+  return (
+    <>
+      <Head>
+        <title>Forage Gourmet Platter Co</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import "../styles/globals.css";
+import "@fontsource/montserrat/100.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/josefin-sans";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
