f300ad75e1b750e6b62177f851bc7959b3dc8f46
Brett <brett@kluske.com>
2022-01-13 05:36:27 +1100

    update hero.js

--- components/hero.js
+++ components/hero.js
@@ -3,26 +3,10 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 // import herobg from "../public/hero.webp";
 // import Image from "next/image";
 
-const Background = () => (
-  <div>
-    <div className='bg-cream -z-10 w-full h-full overflow-hidden'>
-      {/* <Image
-        alt='A table filled with food'
-        src={herobg}
-        layout='fill'
-        objectFit='cover'
-        quality={50}
-        priority='true'
-      /> */}
-    </div>
-  </div>
-);
-
 export default function HeroBox() {
   return (
     <>
-      <div className="grid h-[90vh] w-full bg-cream bg-cover bg-[url('../public/hero.webp')]">
-        <Background />
+      <div className="grid h-[90vh] bg-cream -z-10 w-full overflow-hidden bg-cover bg-[url('../public/hero.webp')]">
         <div className='grid justify-center'>
           <img
             className='grid self-center w-[600px] p-10 z-10'
