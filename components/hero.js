7e6e49c1c86a551764b92073eedb5aae175bedfe
Brett <brett@kluske.com>
2022-01-13 05:15:51 +1100

    testing no next image optimisation

--- components/hero.js
+++ components/hero.js
@@ -1,19 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import herobg from "../public/hero.jpg";
-import Image from "next/image";
+// import herobg from "../public/hero.webp";
+// import Image from "next/image";
 
 const Background = () => (
   <div>
-    <div className='bg-cream overflow-hidden w-full h-full -z-10'>
-      <Image
+    <div className='bg-cream -z-10 w-full h-full overflow-hidden'>
+      {/* <Image
         alt='A table filled with food'
         src={herobg}
         layout='fill'
         objectFit='cover'
         quality={50}
         priority='true'
-      />
+      /> */}
+      <img src='hero.webp' />
     </div>
   </div>
 );
@@ -24,9 +25,15 @@ export default function HeroBox() {
       <div className='grid h-[90vh] w-full bg-cream'>
         <Background />
         <div className='grid justify-center'>
-          <img className='grid self-center w-[600px] p-10 z-10' src='herotext.svg' alt="Something really good is happening soon" />
+          <img
+            className='grid self-center w-[600px] p-10 z-10'
+            src='herotext.svg'
+            alt='Something really good is happening soon'
+          />
         </div>
-        <div className='grid self-end justify-center z-10 text-6xl text-[#fbf7f0]' alt='scroll down'>
+        <div
+          className='grid self-end justify-center z-10 text-6xl text-[#fbf7f0]'
+          alt='scroll down'>
           <FontAwesomeIcon icon={faChevronDown} />
         </div>
       </div>
