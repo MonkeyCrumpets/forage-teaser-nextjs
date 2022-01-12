0f3b20200d8991a40c4e7175b8df65f1b93f6c66
monkeycrumpets <brett@kluske.com>
2022-01-12 05:04:48 +0000

    updated hero.js

--- components/hero.js
+++ components/hero.js
@@ -20,9 +20,9 @@ const Background = () => (
 export default function HeroBox() {
   return (
     <>
-      <div className='h-[90vh] w-full p-5' bg-cream>
+      <div className='grid h-[90vh] w-full p-5' bg-cream>
         <Background />
-        <div className='flex justify-center'>
+        <div className='grid justify-center'>
           <img className='grid self-center w-[550px] z-10' src='herotext.svg' />
         </div>
         <div className='grid self-end justify-center z-10 text-6xl text-[#fbf7f0]' alt='scroll down'>
